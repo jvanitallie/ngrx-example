46a78bdd98522fd850ec7e5d2d6a9fb3ab9eb860
jvanitallie <john.vanitallie@retaildatallc.com>
2021-06-25 17:38:34 -0400

    added min and max and played with css

--- src/app/state/counter.reducer.ts
+++ src/app/state/counter.reducer.ts
@@ -8,14 +8,30 @@ export interface CounterState {
 export const featureKey = 'counter';
 
 export const initialState: CounterState = { count: 0 };
+const minCount: number = 0;
+const maxCount: number = 10000;
 
 // TODO: add the additional reducer functions to react to the appropriate actions
 const _counterReducer = createReducer(
   initialState,
-  on(counterActions.increment, state => ({ ...state, count: state.count + 1 })),
-  on(counterActions.decrement, state => ({ ...state, count: state.count - 1 })),
-  on(counterActions.reset, () => ( initialState )),
-  on(counterActions.setvalue, (state, newValue) => ({ ...state, count: newValue.newValue}))
+  on(counterActions.increment, state => ({
+    ...state,
+    count: state.count + 1 > maxCount ? maxCount : state.count + 1
+  })),
+  on(counterActions.decrement, state => ({
+    ...state,
+    count: state.count - 1 < minCount ? minCount : state.count - 1
+  })),
+  on(counterActions.reset, () => initialState),
+  on(counterActions.setvalue, (state, newValue) => ({
+    ...state,
+    count:
+      newValue.newValue < minCount
+        ? minCount
+        : newValue.newValue > maxCount
+        ? maxCount
+        : newValue.newValue
+  }))
 );
 
 export function counterReducer(

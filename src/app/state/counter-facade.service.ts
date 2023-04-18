69dd9758b39ff104bd6ab6351b6d5efa1bc2ddfb
JVI <john.vanitallie@retaildatallc.com>
2023-04-18 16:41:55 -0400

    Results-2023-04-18

--- src/app/state/counter-facade.service.ts
+++ src/app/state/counter-facade.service.ts
@@ -4,10 +4,11 @@ import { Observable } from 'rxjs';
 import * as counterActions from '../state/counter.actions';
 import * as counterSelectors from '../state/counter.selectors';
 import { CounterState } from './counter.reducer';
+import { CounterService } from '../services/counter.service';
 
 @Injectable()
 export class CounterFacadeService {
-  constructor(private store: Store<CounterState>) {}
+  constructor(private store: Store<CounterState>, private counterService: CounterService) {}
 
   public getCount(): Observable<number> {
     return this.store.pipe(select(counterSelectors.selectCounterValue));
@@ -23,13 +24,16 @@ export class CounterFacadeService {
 
   public resetCount(): void {
     // TODO: Wire this up to use ngRx to reset the state to the initial state
+    this.store.dispatch(counterActions.reset());
   }
 
   public setValue(val: number): void {
     // TODO: Wire this up to pass the value passed in to the state for the current counter value
+    this.store.dispatch(counterActions.setvalue({newValue : val}));
   }
 
   public loadValue(): void {
     // TODO: Wire this up so that the current counter value is loaded from the counter service
+    //this.counterService.loadCounter().subscribe(val => this.store.dispatch(counterActions.loadvalue(val)))
   }
 }
